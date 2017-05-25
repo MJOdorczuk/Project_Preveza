819337e21782b88aa0c3570277ae8a12a13acfe2
student229018 <prawilnaik69@gmail.com>
2017-05-25 16:04:07 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "common.h"
+#include "Server.h"
+
+int main()
+{
+    TCPServer server;
+    server.start(1666);
+    while(server.is_running()) 
+	{ 
+		common.sleep(1);
+		cout << "*";
+	}
+    return 0;
+}
