ca8a6fa0ea7ecf203f5f7af12da793adf192d031
MJOdorczuk <michaljanodorczuk@gmail.com>
2017-06-08 12:04:27 +0200

    Create MyCommands.h

--- MyCommands.h
+++ MyCommands.h
@@ -23,24 +23,18 @@ public:
 };
 
 
-/*
+
 class CmdList : public Command
 {
 public:
-	CmdList(ConnectedClient *client, string t) : Command(client, t) { }
-	CmdList() : Command() { }
-	const string getName() { return "list"; }
-	bool exec();
+	CmdList(const string commandName, const string commandDescription) : Command(commandName, commandDescription) { }
+	bool exec(ConnectedClient *client, vector<string> args);
 };
 
 class CmdMsg : public Command
 {
 public:
-	CmdMsg(ConnectedClient *client, string t) : Command(client, t) { }
-	CmdMsg() : Command() { }
-	const string getName() { return "msg"; }
-	const string getDescription() { return string("usage: <msg> <username> <text to send>"); }
-	bool exec();
+	CmdMsg(const string commandName, const string commandDescription) : Command(commandName, commandDescription) { }
+	bool exec(ConnectedClient *client, vector<string> args);
 };
 
-*/
