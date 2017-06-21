314f5d07ea3013130779ab442d3b2d1955002a16
MJOdorczuk <michaljanodorczuk@gmail.com>
2017-06-21 22:33:34 +0200

    Update GameSocket.cpp

--- GameSocket.cpp
+++ GameSocket.cpp
@@ -1,129 +1,197 @@
-#ifndef GAMESOCKET_H
-#define GAMESOCKET_H
-#include <vector>
-#include <string>
+#include "gamesocket.h"
 
-using namespace std;
 
-class ConnectedClient;
-
-class GameSocket
+GameSocket::GameSocket(ConnectedClient *firstPlayer)
 {
-    private:
+    this->players.push_back(firstPlayer);
+    this->turn = PLAYER1_TURN;
+    this->phase = SETUP_PHASE;
+    this->currentX = 666;
+    this->currentY = 666;
+    this->points[0] = 20;
+    this->points[1] = 20;
+}
 
-        ////
-        //  VARIABLES
-        ////
+GameSocket::~GameSocket()
+{
+    //dtor
+}
 
-        //
-        //  Pointers to ConnectedClient class type objects responsible for connecting players with game
-        //
-        vector <ConnectedClient*> players;
-        //
-        //  The turn variable is information who is supposed to make his movement right now
-        //
-        //  Value of this variable is the number of currently having turn player's pointer in players vector
-        //
-        unsigned short turn;
-        //
-        //  The battletide is an array containing all information about current position of ships, hit points and state of ships
-        //
-        //  First argument corresponds to X-axis position
-        //  Second argument corresponds to Y-axis position
-        //  Third argument corresponds to number of plane (0 - plane of first player, 1 - plane of second player)
-        //
-        //  The array can have four value
-        //      0 - no ship, no hit
-        //      1 - ship, no hit
-        //      2 - hit ship
-        //      3 - hit blank pole
-        //
-        unsigned short battletide[10][10][2];
-        unsigned short ships[2]; //Both are supposed to be changed to vectors
-        unsigned short currentX, currentY;
-        unsigned short phase;
-        unsigned short points[2];
+bool GameSocket::addPlayer(ConnectedClient *secondPlayer)
+{
+    if(this->players.size()>=MAX_PLAYER_NUMBER) return true;
+    this->players.push_back(secondPlayer);
+    return false;
+}
 
-    public:
+unsigned short GameSocket::checkPlayer(ConnectedClient* player)
+{
+    for(int i=0; i<players.size(); i++){if(players.at(i) == player) return i;}
+    return ACCES_DENIED;
+}
 
-        ////
-        //CONSTANTS
-        ////
+void GameSocket::putShip(unsigned short x, unsigned short y, char direction, unsigned short turn)
+{
+    unsigned short dx = 0, dy = 0, length;
+    unsigned short quantity = ships[(turn)%players.size()];
+    if(quantity<4)length = 1;
+    else if(quantity<7)length = 2;
+    else if(quantity<9)length = 3;
+    else length = 4;
+    switch(direction)
+    {
+        case('N'):
+            {
+            dy = -1;
+            break;
+            }
+        case('S'):
+            {
+                dy = 1;
+                break;
+            }
+        case('W'):
+            {
+                dx = -1;
+                break;
+            }
+        case('E'):
+            {
+                dx = 1;
+                break;
+            }
+        default:{break;}
+    }
+    for(int i=0; i<length; i++) battletide[x+dx*i][y+dy*i][turn%players.size()] = NOT_HIT_SHIP;
+}
 
-        const unsigned short MAX_PLAYER_NUMBER = 2, MIN_PLAYER_NUMBER = 2;
-        //
-        //  It's returned by checking current state of chosen field in method ifHit(coordinates);
-        //
-        //  Is returned when there's neither ship nor the field was hit by other player.
-        //
-        const unsigned short NOT_HIT_NO_SHIP = 0;
-        //
-        //  It's returned by checking current state of chosen field in method ifHit(coordinates);
-        //
-        //  Is returned when there's no ship but the field was hit by other player.
-        //
-        const unsigned short HIT_NO_SHIP = 1;
-        //
-        //  It's returned by checking current state of chosen field in method ifHit(coordinates);
-        //
-        //  Is returned when there's ship but the field was not hit by other player.
-        //
-        const unsigned short NOT_HIT_SHIP = 2;
-        //
-        //  It's returned by checking current state of chosen field in method ifHit(coordinates);
-        //
-        //  Is returned when there's ship and the field was hit by other player.
-        //
-        const unsigned short HIT_SHIP = 3;
-        //
-        //  It's returned by method getTurn(turn).
-        //
-        //  Is returned when first player is supposed to make a move.
-        //
-        const unsigned short PLAYER1_TURN = 0;
-        //
-        //  It's returned by method getTurn(turn).
-        //
-        //  Is returned when second player is supposed to make a move.
-        //
-        const unsigned short PLAYER2_TURN = 1;
+unsigned short GameSocket::ifHit(unsigned short x, unsigned short y)
+{
+    if(x>9 || y>9) return NOT_AVAILABLE;
+    unsigned short condition = battletide[x][y][(turn+1)%players.size()];
+    if(condition == NOT_HIT_NO_SHIP) return MISS;
+    if(condition == HIT_NO_SHIP || condition == HIT_SHIP) return NOT_AVAILABLE;
+    if(condition == NOT_HIT_SHIP) return HIT;
+    else return ERR;
+}
 
-        const unsigned short MISS = 0, HIT = 1, NOT_AVAILABLE = 2, WIN = 3, ERR = 666, AVAILABLE = 1, ACCES_DENIED = 404;
-        const unsigned short SETUP_PHASE = 0, BATTLE_PHASE = 1;
+unsigned short GameSocket::ifSurroundingClear(unsigned short x, unsigned short y, unsigned short turn)
+{
+    unsigned short i, j, condition;
+    if(x>9 || y>9 || turn>=MAX_PLAYER_NUMBER) return NOT_AVAILABLE;
+    if(x==0) i=0;
+    else i=x-1;
+    if(y==0) j=0;
+    else j=y-1;
+    for(;i<x+1-x%9;i++)
+        for(;j<y+1-y%9;y++)
+        {
+            condition = battletide[i][j][(turn)%players.size()];
+            if(condition == HIT_NO_SHIP || condition == HIT_SHIP || condition == NOT_HIT_SHIP) return NOT_AVAILABLE;
+        }
+    return AVAILABLE;
+}
 
-        ////
-        //  METHODS
-        ////
+unsigned short GameSocket::ifDirectionCorrect(char direction, unsigned short x, unsigned short y, unsigned short turn)
+{
+    if(x>9 || y>9 || (direction != 'N' && direction != 'S' && direction != 'W' && direction != 'E' && direction != 'U')) return NOT_AVAILABLE;
+    unsigned short condition = battletide[x][y][(turn)%players.size()];
+    unsigned short quantity = ships[(turn)%players.size()];
+    unsigned short dx = 0, dy = 0, length;
+    if(quantity<4)length = 1;
+    else if(quantity<7)length = 2;
+    else if(quantity<9)length = 3;
+    else length = 4;
+    switch(direction)
+    {
+        case('N'):
+            {
+            dy = -1;
+            break;
+            }
+        case('S'):
+            {
+                dy = 1;
+                break;
+            }
+        case('W'):
+            {
+                dx = -1;
+                break;
+            }
+        case('E'):
+            {
+                dx = 1;
+                break;
+            }
+        default:{break;}
+    }
+    if(direction == 'U')
+    {
+        if(condition == NOT_HIT_NO_SHIP) return AVAILABLE;
+        if(condition == HIT_NO_SHIP || condition == HIT_SHIP || condition == NOT_HIT_SHIP) return NOT_AVAILABLE;
+    }
+    if(condition == NOT_HIT_NO_SHIP || condition == NOT_HIT_SHIP)
+    {
+        for(int i=0; i<length; i++){if(ifSurroundingClear(x+i*dx,y+i*dy,this->turn) == NOT_AVAILABLE) return NOT_AVAILABLE;}
+        return AVAILABLE;
+    }
+    if(condition == HIT_NO_SHIP || condition == HIT_SHIP) return NOT_AVAILABLE;
+    else return ERR;
+}
 
-        //
-        //  Constructor assigns pointer to first player who starts the game
-        //
-        GameSocket(ConnectedClient *firstPlayer);
-        //
-        //  Virtual destructor of GameSocket
-        //
-        virtual ~GameSocket();
-        //
-        //  AddPlayer method sets pointer for second player.
-        //
-        //  It returns information if second player is already in game (false - is not, true - is)
-        //
-        bool addPlayer(ConnectedClient *secondPlayer);
-        unsigned short makeMove(string command, ConnectedClient *Player);
-        unsigned short getPlayersNumber(){return players.size();}
-        vector <ConnectedClient*> getPlayers(){return players;}
-        ConnectedClient* getTurn(){return players.at(turn);}
-    protected:
-    private:
-        //
-        //  DrawShips method initiates methods responsible for displaying whole game onto screen
-        //
-        void drawShips();
-        unsigned short ifHit(unsigned short x, unsigned short y);
-        unsigned short ifDirectionCorrect(char direction, unsigned short x, unsigned short y, unsigned short turn);
-        unsigned short ifSurroundingClear(unsigned short x, unsigned short y, unsigned short turn);
-        void putShip(unsigned short x, unsigned short y, char direction, unsigned short turn);
-        unsigned short checkPlayer(ConnectedClient* player);
-};
-
-#endif // GAMESOCKET_H
+unsigned short GameSocket::makeMove(string command, ConnectedClient* Player)
+{
+    if(players.size() < MIN_PLAYER_NUMBER) return NOT_AVAILABLE;
+    if(this->phase != SETUP_PHASE && Player != players.at(this->turn)) return NOT_AVAILABLE;
+    unsigned short turn = checkPlayer(Player);
+    if(turn == ACCES_DENIED) return ACCES_DENIED;
+    unsigned short x = this->currentX, y = this->currentY;
+    if(command == "N" || command == "S" || command == "W" || command == "E")
+    {
+        if(x > 9 || y > 9) return NOT_AVAILABLE;
+        else if(ifDirectionCorrect(command.at(0),x,y,turn) != AVAILABLE || this->ships[turn]<9) return NOT_AVAILABLE;
+        else
+        {
+            putShip(x,y,command.at(0),turn);
+            this->ships[turn]++;
+            for(int i = 0; i < players.size(); i++) if(this->ships[i]<10) return HIT;
+            this->phase = BATTLE_PHASE;
+            return HIT;
+        }
+    }
+    if(command.at(0)<'A' || command.at(0)>'J' || command.at(1)<'0' || command.at(1)>'9') return NOT_AVAILABLE;
+    x = (unsigned short)(command.at(0) - 'A');
+    y = (unsigned short)(command.at(1) - '0');
+    if(this->phase == SETUP_PHASE)
+    {
+        if(x > 9 || y > 9) return NOT_AVAILABLE;
+        else if(ifDirectionCorrect('U',x,y,turn) != AVAILABLE) return NOT_AVAILABLE;
+        this->currentX = x;
+        this->currentY = y;
+        return HIT;
+    }
+    else if(this->phase == BATTLE_PHASE)
+    {
+        if(x > 9 || y > 9) return NOT_AVAILABLE;
+        unsigned short shipstatus = ifHit(x,y);
+        if(shipstatus == ERR) return ERR;
+        else if(shipstatus == NOT_AVAILABLE) return NOT_AVAILABLE;
+        else if(shipstatus == MISS)
+        {
+            this->battletide[x][y][(turn+1)%players.size()] = HIT_NO_SHIP;
+            this->turn = (this->turn+1)%players.size();
+            return MISS;
+        }
+        else if(shipstatus == HIT)
+        {
+            this->battletide[x][y][(turn+1)%players.size()] = HIT_SHIP;
+            this->points[(turn+1)%players.size()]--;
+            if(this->points[(turn+1)%players.size()] == 0) return WIN;
+            this->turn = (this->turn+1)%players.size();
+            return HIT;
+        }
+        else return ERR;
+    }
+    else return ERR;
+}
